30dcf45a1e626a55a730093a388d66b5a153dfaa
Emile Cadorel <ecadorel@gmail.com>
2016-09-21 17:16:46 +0200

    commit de ce que j'ai fait tout a l'heure

--- source/app.d
+++ source/app.d
@@ -1,54 +1,25 @@
 import std.stdio;
-import http.HttpRequest;
-import http.HttpServer;
-import http.HttpSession;
-import http.HttpResponse;
-import std.string, std.conv;
-import utils.LexerString;
-import std.container;
-import std.socket;
+import control.Session;
 
+class A {
+    string toString () {
+	return "A";
+    }    
+}
 
-class Session : HttpSession {
-
-    this (Socket socket) {
-	super (socket);
-    }
-
-    HttpRequest toRequest (string value) {
-	//TODO, transformation de la chaine en requete http
-	LexerString lex = new LexerString (value);
-	lex.setKeys (make!(Array!string)([":", ",", " ", "\n", "\r"]));
-	lex.setSkip (make!(Array!string)([" ", "\n", "\r"]));
-	Word word;
-	return HttpRequestParser.parser (lex);
-    }
-
-    void [] recv_all () {
-	byte [] total;
-	while (true) {
-	    byte [] data;
-	    data.length = 256;
-	    auto length = this.socket.receive (data);
-	    total ~= data;
-	    if (length < 256) return total;
-	}
-    }
-    
-    void on_begin (Address client) {
-	writeln (client.toAddrString(), ":", client.toPortString ());
-	//reception de la requete http
-	auto data = this.recv_all ();
-	auto request = toRequest (cast(string) data);
-	writeln (request.toString);
-	this.socket.send ("bonjour");
-    }
-
-    void on_end () {
-    }
-
+void fill_session (Session session) {
+    session["salut"] = new A ();
+    session ["comment"] = new int(89);
 }
 
 void main (string [] args) {
-    HttpServer!Session session = new HttpServer!Session (args);
+    Session session = new Session;
+    fill_session (session);
+    auto res = session.get!A ("salut");
+    if (res !is null)
+	writeln (res.toString);
+
+    auto b = session.get!int ("comment");
+    if (b !is null)
+	writeln (*b);
 }
--- source/control/Session.d
+++ source/control/Session.d
@@ -4,17 +4,27 @@ module control.Session;
 class Session {
 
     void opIndexAssign (T : Object) (T elem, string name) {
-	datas[name] = cast(Object) (elem);
+	datas[name] = cast(void*)elem;
     }
     
+    void opIndexAssign (T) (T * elem, string name) {
+	datas[name] = elem;
+    }
+
     T get (T : Object) (string name) {
 	auto elem = (name in datas);
 	if (elem is null) return null;
-	else return cast(T) *elem;
+	else return cast (T) (*elem);
+    }
+    
+    T * get (T) (string name) {
+	auto elem = (name in datas);
+	if (elem is null) return null;
+	else return cast (T*) (*elem);
     }
     
     private {
-	Object [string] datas;
+	void* [string] datas;
     }
     
 }
