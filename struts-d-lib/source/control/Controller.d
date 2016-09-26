16e47e002687b80e5a7c90c6f5bcd3ef9b53edf3
GuillaumeGas <guillaume@gas-ntic.fr>
2016-09-26 18:19:47 +0200

    Exemple d'utilisation des variables get dans un controleur simple

--- struts-d-lib/source/control/Controller.d
+++ struts-d-lib/source/control/Controller.d
@@ -3,16 +3,36 @@ import http.request;
 
 abstract class Controller {
 
-    this () {}
+  this () {}
 
-    /**
+  /**
      Unpack la request et rempli les attributs du controller en consequence
-     */
-    void unpackRequest (HttpRequest request) {
-    }
+  */
+  void unpackRequest (HttpRequest request) {
+    this._request = request;
+  }
 
-    abstract string execute ();
-    
-    ~this () {}
-    
+  abstract string execute ();
+
+  ~this () {}
+
+  HttpParameter get (string key) {
+    return this._request.url.param(key);
+  }
+
+  HttpParameter post (string key) {
+    return this._request.post_values()[key];
+  }
+
+  HttpParameter cookie (string key) {
+    return this._request.cookies()[key];
+  }
+
+  ref HttpRequest request () {
+    return this._request;
+  }
+
+  private {
+    HttpRequest _request;
+  }
 }
